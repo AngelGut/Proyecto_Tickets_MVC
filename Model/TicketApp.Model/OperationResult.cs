62878509efde586f144eb19c8ff1253ab8a2810c
brailyn1905 <ramirezbrailyn55@gmail.com>
2026-01-25 15:56:54 -0400

    Me encargué de la implementación de la capa Modelo, creando la entidad Ticket y el enumerador TicketStatus para representar los datos y estados del sistema. También desarrollé un Store en memoria que permite crear, consultar y actualizar tickets durante la ejecución de la aplicación.
    Adicionalmente, se incluyó la clase genérica OperationResult<T> para estandarizar la comunicación entre el controlador y la vista, manteniendo la lógica de negocio desacoplada de la interfaz gráfica.

---
+++ Model/TicketApp.Model/OperationResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TicketApp.Model.Common
+{
+
+    // esto lo usamos para  comunicar información entre el Controller y la View
+    //sin acoplar la lógica de negocio a la interfaz gráfica.
+
+
+
+    public class OperationResult<T>
+    {
+        //usamos private set para que solo se pueda modificar desde dentro de la clase o en metodos comtrolados 
+        public bool Success { get; private set; }
+        public string Message { get; private set; }
+        public T? Data { get; private set; }
+
+        private OperationResult(bool success, string message, T? data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+        public static OperationResult<T> Ok(T data, string message = "Operación exitosa")
+        {
+            return new OperationResult<T>(true, message, data);
+        }
+
+        public static OperationResult<T> Fail(string message)
+        {
+            return new OperationResult<T>(false, message, default);
+        }
+    }
+}
---
+++ Model/TicketStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace TicketApp.Model.Enums
+{
+    //aque hacemos un enum esto para saver el estado del ticket
+    // donde 0 sera abierto y 1  cerrado
+    public enum TicketStatus
+    {
+        Disponible = 0,
+        Ocupado = 1
+
+    }
+}
