882ec96b119b39d46307e175675ad32ce2b6605a
David Paez <jeffersonpaez5@gmail.com>
2026-01-25 17:05:57 -0400

    Creacion de las Clases Ticket y TicketStatus, Ticket Status con el Enum encargado del estado de los tickes, y Ticket con todas las Propiedades del Ticket y el metodo para cerrar los tickets y una validacion basica

---
+++ Model/TicketStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Model
+{
+    /// <summary>
+    /// Representara los estados posibles de un Ticket dentro del sistema, digase Cerrado o Abierto
+    /// El uso del enum nos dejara controlar y restringir los valores validos que puede tener el estado de un ticket
+    /// que puede tener el estado de un ticket.
+    /// </summary>
+    
+    public enum TicketStatus
+    {
+        //TODO: Abiero representa un ticket que esta en proceso de resolucion, Cerrado representa un ticket que ya fue resuelto
+        Abierto,
+        Cerrado
+    }
+}
